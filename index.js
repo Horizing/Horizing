257c0e1e497a3faad397d8b4e0b581089d5a1ecb
Theudo Tsai <38889573+VoluntieTsai@users.noreply.github.com>
2024-01-17 14:44:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,16 +18,13 @@ const readme = `\
 
 <img align="left" src="https://github.com/VoluntieTsai/VoluntieTsai/blob/main/Elias.png" height="225" width="auto" >  
 <br/><br/><br/>
-🤝Credit is productivity.  
-
-❤️The things you chain yourself to are the things that set you free.
-
-🛡️Guarding the bottom line of everything.  
-
-🎭Culture is indescribable.  
-
+🤝Credit is productivity.<br/>
+🌎Don't ignore kind people, Even if only malicious voices remain.<br/>
+❤️The things you chain yourself to are the things that set you free.<br/>
+🛡️Guarding the bottom line of everything.<br/>
+🎭Culture is indescribable.<br/>
 🍃Anger is easy, but same to forget.
-<br/><br/><br/><br/>
+<br/><br/><br/>
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 ---
