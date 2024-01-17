eb5a68fd199b2b62ea7b1fb17ac0b052ed1ca1c2
Theudo Tsai <38889573+VoluntieTsai@users.noreply.github.com>
2024-01-17 17:00:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### ⚗️ Learning is for The Courage With Other People.
+## ⚗️ Learning is for The Courage With Other People.
 
 <img align="left" src="https://github.com/VoluntieTsai/VoluntieTsai/blob/main/Elias.png" height="225" width="auto" >  
 <br/><br/><br/>
@@ -24,16 +24,15 @@ const readme = `\
 🌎Even if only malicious voices remain, don't ignore kind people.<br/>
 🛡️Guarding the bottom line of everything.<br/>
 🍃Anger is easy, but same to forget.
-<br/><br/><br/>
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+<br/><br/>
+
+---
+### ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+⏰ Updated on Wed, 17 Jan 2024 08:50:47 GMT
 
 ---
 
-⏰ Updated on ${new Date().toUTCString()}
-
----
-
-📚Books written by me<br/><br/>
+### 📚Small Books written by me<br/><br/>
 ![Progress Bar CI By liununu](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)\
 `
 
