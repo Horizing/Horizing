666c71dc1161f03e9305489c59ee6721e4963bcc
Theudo Tsai <38889573+VoluntieTsai@users.noreply.github.com>
2024-01-17 14:47:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const readme = `\
 <img align="left" src="https://github.com/VoluntieTsai/VoluntieTsai/blob/main/Elias.png" height="225" width="auto" >  
 <br/><br/><br/>
 🤝Credit is productivity.<br/>
-🌎Don't ignore kind people, Even if only malicious voices remain.<br/>
 ❤️The things you chain yourself to are the things that set you free.<br/>
+🌎Don't ignore kind people, Even if only malicious voices remain.<br/>
 🛡️Guarding the bottom line of everything.<br/>
 🎭Culture is indescribable.<br/>
 🍃Anger is easy, but same to forget.
