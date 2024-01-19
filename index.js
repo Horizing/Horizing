f5ba57a09bc14c25bb96e7a4e9dba118cd3a7523
Theudo Tsai <38889573+VoluntieTsai@users.noreply.github.com>
2024-01-20 03:52:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ const readme = `\
 🤝Credit is productivity.<br/>
 🎭Don't be too rational to enjoy things like art in culture, just feel it.<br/>
 👻Life and education need humor and entertainment too.<br/>
+💬Infer by essence and information gap to face the confusion and anxiety.<br/>
 ❤️The things you chain yourself to, are the things that set you free.<br/>
 🌎Even if only malicious voices remain, don't ignore kind people.<br/>
-🛡️Guarding the bottom line of everything.<br/>
-🍃Anger is easy, but same to forget.
+🕯️Time flies so fast, we will meet again.
 <br/><br/>
 
 ---
@@ -35,6 +35,9 @@ const readme = `\
 🎮[Steam](https://steamcommunity.com/profiles/76561198179920187) 🗒️[Blog](https://steamcommunity.com/profiles/76561198179920187) 🏠[Home](https://steamcommunity.com/profiles/76561198179920187) More🍀[Linktree](https://linktr.ee/)
 ## 💡 I Hope These Will Help You.
 ### 📚Small Books written by me<br/><br/>
+🍃Anger is easy, but same to forget.<br/>
+🛡️Guarding the bottom line of everything.<br/>
+🔥Let's do something for our lovely worlds, again and again.<br/><br/>
 ![Progress Bar CI By liununu](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)\
 `
 
