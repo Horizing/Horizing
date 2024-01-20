44fa52661ac49cda57acf2f4a63a4fea8c7caa6f
Theudo Tsai <38889573+VoluntieTsai@users.noreply.github.com>
2024-01-20 18:02:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ const readme = `\
 ❤️The things you chain yourself to, are the things that set you free.  
 🌎Even if only malicious voices remain, don't ignore kind people.  
 🕯️Time flies so fast, we will meet again.  
-<br/><br/>
+
+“Life will offer you a diminishing number of opportunities to show how smart you are.”  
+“It will offer an infinite number of occasions that require kindness, mercy, grace, sensitivity, sympathy, generosity and love.”
 
 ---
 ### ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
