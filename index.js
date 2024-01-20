ed1f94a1e3a96e6d9b3660d7ef17bf6e7e1e77db
Theudo Tsai <38889573+VoluntieTsai@users.noreply.github.com>
2024-01-20 19:02:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ const readme = `\
 🌎Even if only malicious voices remain, don't ignore kind people.  
 🕯️Time flies so fast, we will meet again.  
 <br/><br/>
-***“Life will offer you a diminishing number of opportunities to show how smart you are, ”***🍋  
-***“But it will offer an infinite number of occasions that require kindness, mercy, grace, sensitivity, sympathy, generosity and love.”***🍬
+***“Life will offer you a diminishing number of opportunities to show how smart you are, ”***  
+***“But it will offer an infinite number of occasions that require kindness, mercy, grace, sensitivity, sympathy, generosity and love.”***
 
 ---
 ### ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
