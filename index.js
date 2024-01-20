2221d98326b32cb7a5428b6fbe02416d00f3cb68
Theudo Tsai <38889573+VoluntieTsai@users.noreply.github.com>
2024-01-20 23:11:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const readme = `\
 ### 🌱 Hi, Lost lambs 😈<br/><br/>👁️The Self of us will one day be strong enough to influence others.<br/>Can We Control That?👿
 <img align="left" src="https://github.com/VoluntieTsai/VoluntieTsai/blob/main/Elias.png" height="225" width="auto" ><br/><br/><br/>
 🤝Credit is productivity.  
-🎭If you're too rational to enjoy things like art in [culture](https://steamcommunity.com/profiles/76561198179920187), just feel it. —***[My view of 魔法使いの夜](https://steamcommunity.com/profiles/76561198179920187)***   
+🎭Don't be too rational to enjoy things like art in [culture](https://steamcommunity.com/profiles/76561198179920187), just feel it. —***[My view of 魔法使いの夜](https://steamcommunity.com/profiles/76561198179920187)***   
 👻Life and education need humor and entertainment too.  
 💬Infer by essence and information gap to face the confusion and anxiety.  
 ❤️The things you chain yourself to, are the things that set you free.  
