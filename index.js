8a5b3669757dedd14040177a672166620014e1fc
Theudo Tsai <38889573+VoluntieTsai@users.noreply.github.com>
2024-01-22 22:05:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,11 @@ const readme = `\
 **Reviews**(In no particular order LOL)  
 | [境界線上のホライゾン](AAAA)  | 💠[Type-Moon @My view](AAAA), [魔法使いの夜](AAAA), [空の境界](AAAA) | [Others](AAAA) |  
 
+**Cognition-认知**  
+| 从游戏聊起的概念有点怪 | 时间与体感的老化 | 高估与低估所处时代进程 |
+
 **Guides**  
-| [烹饪](AAAA) | [二手市场](AAAA) |  
+| [炼药与烹饪](AAAA) | [高价优质的产品怎么找](AAAA) | [二手市场](AAAA) |  
 
 Tech (sort by considered)  
 
